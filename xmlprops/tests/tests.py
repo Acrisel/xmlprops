2e9b65bbb72893e156d5749febf972baad754753
Uri <uri@acrisel.com>
2015-10-18 19:28:25 -0500

    Add unit-tests

--- xmlprops/tests/tests.py
+++ xmlprops/tests/tests.py
@@ -5,7 +5,10 @@ Created on Sep 28, 2015
 '''
 
 import unittest
-from xmlprops import XMLPropsStr as XMLProps
+import sys
+import os
+from xmlprops import XMLPropsStr
+from xmlprops import XMLPropsFile
 
 props_xml_01='''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
@@ -37,18 +40,66 @@ props_xml_01='''<?xml version="1.0" encoding="UTF-8"?>
 <entry key="db1.use_unicode">True</entry>
 <entry key="db1.charset">utf8</entry>
 <entry key="db1.priority">1</entry>
+<entry key="db1.priority">2</entry>
 
 </properties>
 '''
 
 class TestXMLProps(unittest.TestCase):
 
-    def test_get_match(self):
-        xmlprops=XMLProps(props=props_xml_01)
+    def test_get_match_str(self):
+        xmlprops=XMLPropsStr(props=props_xml_01)
         
         self.assertEqual(xmlprops.get_match(key_prefix='db1.')['password'], 'passworddb')
         self.assertEqual(xmlprops.get_match(key_prefix='db1.password')[''], 'passworddb')
+    
+    def test_get_order_match_str(self):
+        xmlprops=XMLPropsStr(props=props_xml_01)
+        
+        self.assertEqual(xmlprops.get_match(key_prefix='db1.')['priority'], '2')
+        self.assertEqual(xmlprops.get_match(key_prefix='db1.priority')[''], '2')
+    
+    def test_get_match_file(self):
+        """ 
+        Write configuration definition to file and unit-test XMLPropsFile
+        This is to keep consistency with unit-tests for XMLPropsStr
+        """
+        config_file_name = "props_unit_test.xml"
+        try:
+            target_file = open(config_file_name,"w")
+            target_file.write(props_xml_01)
+            target_file.close()
+        except Exception as e:
+                raise Exception('Failed to create and write to file: {}; {}; {}'.format(os.getcwd(),config_file_name, repr(e)))
+
+        xmlprops=XMLPropsFile(props=config_file_name)
+        os.remove(config_file_name)
+        
+        self.assertEqual(xmlprops.get_match(key_prefix='db1.')['password'], 'passworddb')
+        self.assertEqual(xmlprops.get_match(key_prefix='db1.password')[''], 'passworddb')
+        
+    def test_get_order_match_file(self):
+        """ 
+        Write configuration definition to file and unit-test XMLPropsFile
+        This is to keep consistency with unit-tests for XMLPropsStr
+        """
+        config_file_name = "props_unit_test.xml"
+        try:
+            target_file = open(config_file_name,"w")
+            target_file.write(props_xml_01)
+            target_file.close()
+        except Exception as e:
+                raise Exception('Failed to create and write to file: {}; {}; {}'.format(os.getcwd(),config_file_name, repr(e)))
+
+        xmlprops=XMLPropsFile(props=config_file_name)
+        os.remove(config_file_name)
+        
+        self.assertEqual(xmlprops.get_match(key_prefix='db1.')['priority'], '2')
+        self.assertEqual(xmlprops.get_match(key_prefix='db1.priority')[''], '2')
 
 
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestXMLProps))
+    unittest.TextTestRunner(verbosity=2, stream=sys.stdout).run(suite)
