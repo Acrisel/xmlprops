62cad1669f43c3f4baa1cb3b7ff670228e1c6053
Uri <uri@acrisel.com>
2016-02-07 10:45:51 -0600

    .

--- xmlprops/setup.py
+++ xmlprops/setup.py
@@ -23,7 +23,7 @@ if "install" in sys.argv:
             break
 
 setup_info={'name': 'xmlprops',
- 'version': '1.0.2',
+ 'version': '2.0.0',
  'url': 'https://github.com/Acrisel/xmlprops',
  'author': 'Acrisel Team',
  'author_email': 'support@acrisel.com',
